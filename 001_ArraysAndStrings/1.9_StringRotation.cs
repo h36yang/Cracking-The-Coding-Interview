a2a63d87afdc533b41cf374b65138af6a6937520
Jack Yang <h36yang@gmail.com>
2019-09-03 21:48:22 -0400

    Question 1.9

---
+++ 001_ArraysAndStrings/1.9_StringRotation.cs
@@ -0,0 +1,35 @@
+﻿namespace _001_ArraysAndStrings
+{
+    /// <summary>
+    /// 1.9) String Rotation:
+    /// Assume you have a method isSubstring which checks if one word is a substring of another.
+    /// Given two strings, s1 and s2, write code to check if s2 is a rotation of s1 using only one call to isSubstring.
+    /// (e.g. "waterbottle" is a rotation of"erbottlewat")
+    /// </summary>
+    public class Question_1_9
+    {
+        /// <summary>
+        /// Duplicate str1 and check if it contains str2
+        /// <para>Time Complexity: O(n), where n is length of the string</para>
+        /// <para>Space Complexity: O(n)</para>
+        /// </summary>
+        /// <param name="str1"></param>
+        /// <param name="str2"></param>
+        /// <returns></returns>
+        public static bool CheckStringRotation(string str1, string str2)
+        {
+            if (string.IsNullOrEmpty(str1) || string.IsNullOrEmpty(str2) || str1.Length != str2.Length)
+            {
+                return false;
+            }
+
+            if (str1.Equals(str2))
+            {
+                return true;
+            }
+
+            string str1str1 = str1 + str1;
+            return str1str1.Contains(str2);
+        }
+    }
+}
---
+++ 001_ArraysAndStringsTest/1.9_StringRotationTest.cs
@@ -0,0 +1,34 @@
+﻿using _001_ArraysAndStrings;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace _001_ArraysAndStringsTest
+{
+    [TestClass]
+    public class Question_1_9_Test
+    {
+        [DataTestMethod]
+        [DataRow("a", "a")]
+        [DataRow("ab", "ba")]
+        [DataRow("abc", "abc")]
+        [DataRow("abc", "bca")]
+        [DataRow("aaabbbccc", "aabbbccca")]
+        [DataRow("waterbottle", "erbottlewat")]
+        public void CheckStringRotationTest_ReturnTrue(string str1, string str2)
+        {
+            bool result = Question_1_9.CheckStringRotation(str1, str2);
+            Assert.IsTrue(result, $"{str1} is not rotation of {str2}.");
+        }
+
+        [DataTestMethod]
+        [DataRow("a", "")]
+        [DataRow("a", "b")]
+        [DataRow("ab", "abc")]
+        [DataRow("abcd", "acbd")]
+        [DataRow("waterbottle", "erbottleway")]
+        public void CheckStringRotationTest_ReturnFalse(string str1, string str2)
+        {
+            bool result = Question_1_9.CheckStringRotation(str1, str2);
+            Assert.IsFalse(result, $"{str1} is rotation of {str2}.");
+        }
+    }
+}
