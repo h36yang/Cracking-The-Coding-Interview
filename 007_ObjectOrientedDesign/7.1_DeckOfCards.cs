0b8f85ec7a63f10a82801bd010a6517c09238956
Jack Yang <h36yang@gmail.com>
2019-10-20 22:40:38 -0400

    Question 7.1

---
+++ 007_ObjectOrientedDesign/7.1_DeckOfCards.cs
@@ -0,0 +1,243 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _007_ObjectOrientedDesign
+{
+    /// <summary>
+    /// 7.1) Deck of Cards:
+    /// Design the data structures for a generic deck of cards.
+    /// Explain how you would subclass the data structures to implement blackjack.
+    /// </summary>
+    public static class Question_7_1
+    {
+        #region Generic Data Structures
+
+        public enum CardSuit
+        {
+            Spade,
+            Heart,
+            Club,
+            Diamond
+        }
+
+        public abstract class Card
+        {
+            public CardSuit Suit { get; private set; }
+
+            private readonly Dictionary<int, string> _specialValueNameMap = new Dictionary<int, string>()
+            {
+                { 1, "A" },
+                { 11, "J" },
+                { 12, "Q" },
+                { 13, "K" }
+            };
+
+            private readonly Dictionary<CardSuit, string> _suitMap = new Dictionary<CardSuit, string>()
+            {
+                { CardSuit.Spade, "♠" },
+                { CardSuit.Heart, "♥" },
+                { CardSuit.Club, "♣" },
+                { CardSuit.Diamond, "♦" }
+            };
+
+            private readonly int _value;
+
+            public virtual int Value => _value;
+
+            public bool IsAce => _value == 1;
+
+            public Card(CardSuit suit, int value)
+            {
+                Suit = suit;
+                _value = value;
+            }
+
+            public override bool Equals(object obj)
+            {
+                if (!(obj is Card other))
+                {
+                    return false;
+                }
+
+                return Suit.Equals(other.Suit) && Value.Equals(other.Value);
+            }
+
+            public override int GetHashCode()
+            {
+                return Suit.GetHashCode() * 13 + Value.GetHashCode();
+            }
+
+            public override string ToString()
+            {
+                string valueName;
+                if (_specialValueNameMap.ContainsKey(_value))
+                {
+                    valueName = _specialValueNameMap[_value];
+                }
+                else
+                {
+                    valueName = _value.ToString();
+                }
+                return $"{_suitMap[Suit]}{valueName}";
+            }
+        }
+
+        public class Deck<T> where T : Card
+        {
+            private static Random _randomGenerator;
+
+            public List<T> Cards { get; private set; }
+
+            public List<T> DealtCards { get; private set; } = new List<T>(52);
+
+            public int RemainingCards => Cards.Count;
+
+            public int TotalCards => Cards.Count + DealtCards.Count;
+
+            public Deck(List<T> cards)
+            {
+                Cards = new List<T>(cards);
+            }
+
+            public void ResetDeck()
+            {
+                Cards.AddRange(DealtCards);
+                DealtCards.Clear();
+            }
+
+            public void Shuffle()
+            {
+                if (_randomGenerator == null)
+                {
+                    _randomGenerator = new Random();
+                }
+                Cards = Cards.OrderBy(x => _randomGenerator.Next()).ToList();
+            }
+
+            public T DealCard()
+            {
+                if (RemainingCards == 0)
+                {
+                    return null;
+                }
+
+                int lastCardIndex = RemainingCards - 1;
+                T card = Cards[lastCardIndex];
+                Cards.RemoveAt(lastCardIndex);
+                DealtCards.Add(card);
+                return card;
+            }
+
+            public List<T> DealHand(int numberOfCards)
+            {
+                var hand = new List<T>(numberOfCards);
+                for (int i = 0; i < numberOfCards; i++)
+                {
+                    hand.Add(DealCard());
+                }
+                return hand;
+            }
+        }
+
+        public abstract class Hand<T> where T : Card
+        {
+            public List<T> Cards { get; private set; }
+
+            public Hand(List<T> cards)
+            {
+                Cards = new List<T>(cards);
+            }
+
+            public virtual int Score()
+            {
+                int score = 0;
+                foreach (T card in Cards)
+                {
+                    score += card.Value;
+                }
+                return score;
+            }
+
+            public void AddCard(T card)
+            {
+                Cards.Add(card);
+            }
+
+            public override string ToString()
+            {
+                var hand = new StringBuilder();
+                foreach (T card in Cards)
+                {
+                    hand.Append($"{card} ");
+                }
+                return hand.ToString();
+            }
+        }
+
+        #endregion
+
+        #region Blackjack Implementation
+
+        public class BlackjackCard : Card
+        {
+            public override int Value => base.Value <= 10 ? base.Value : 10;
+
+            public int MaxValue => IsAce ? 11 : Value;
+
+            public BlackjackCard(CardSuit suit, int value) : base(suit, value) { }
+        }
+
+        public class BlackjackHand : Hand<BlackjackCard>
+        {
+            public bool IsBusted => Score() > 21;
+
+            public bool Is21 => Score() == 21;
+
+            public bool IsBlackjack => Is21 && Cards.Count == 2;
+
+            public BlackjackHand(List<BlackjackCard> cards) : base(cards) { }
+
+            public override int Score()
+            {
+                List<int> possibleScores = PossibleScores();
+                int finalScore = 0;
+                int minScore = int.MaxValue;
+                foreach (int score in possibleScores)
+                {
+                    if (score > finalScore && score <= 21)
+                    {
+                        finalScore = score;
+                    }
+
+                    if (score < minScore)
+                    {
+                        minScore = score;
+                    }
+                }
+                return finalScore != 0 ? finalScore : minScore;
+            }
+
+            private List<int> PossibleScores()
+            {
+                var scores = new List<int>();
+                for (int i = 0; i < Cards.Count; i++)
+                {
+                    int score = Cards[i].MaxValue;
+                    for (int j = 0; j < Cards.Count; j++)
+                    {
+                        if (i != j)
+                        {
+                            score += Cards[j].Value;
+                        }
+                    }
+                    scores.Add(score);
+                }
+                return scores.Distinct().ToList();
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ 007_ObjectOrientedDesignTest/7.1_DeckOfCardsTest.cs
@@ -0,0 +1,102 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using static _007_ObjectOrientedDesign.Question_7_1;
+
+namespace _007_ObjectOrientedDesignTest
+{
+    [TestClass]
+    public class Question_7_1_Test
+    {
+        [DataTestMethod]
+        [DataRow(0)]
+        [DataRow(1)]
+        [DataRow(2)]
+        [DataRow(3)]
+        [DataRow(4)]
+        [DataRow(5)]
+        [DataRow(6)]
+        [DataRow(7)]
+        [DataRow(8)]
+        [DataRow(9)]
+        public void BlackjackTest(int _)
+        {
+            // Arrange new deck
+            var cards = new List<BlackjackCard>(52);
+            for (int suit = 0; suit < 4; suit++)
+            {
+                for (int value = 1; value <= 13; value++)
+                {
+                    cards.Add(new BlackjackCard((CardSuit)suit, value));
+                }
+            }
+            var deck = new Deck<BlackjackCard>(cards);
+
+            // Assert new deck
+            Assert.AreEqual(52, deck.TotalCards, "Incorrect total number of cards in the initial deck.");
+            Assert.AreEqual(52, deck.RemainingCards, "Incorrect remaining number of cards in the initial deck.");
+            Assert.IsTrue(cards.SequenceEqual(deck.Cards));
+
+            // Act 1
+            deck.Shuffle();
+            List<BlackjackCard> handOfCards = deck.DealHand(2);
+            var hand = new BlackjackHand(handOfCards);
+
+            // Assert new hand
+            Assert.AreEqual(52, deck.TotalCards, "Incorrect total number of cards in the deck.");
+            Assert.AreEqual(50, deck.RemainingCards, "Incorrect remaining number of cards in the deck.");
+            Assert.IsTrue(handOfCards.SequenceEqual(hand.Cards));
+
+            int expectedScore = handOfCards[0].MaxValue + handOfCards[1].MaxValue;
+            if (handOfCards[0].IsAce && handOfCards[1].IsAce)
+            {
+                expectedScore = 12;
+            }
+            Assert.AreEqual(expectedScore, hand.Score(), "Incorrect score calculated.");
+            Assert.AreEqual(expectedScore > 21, hand.IsBusted, "Check IsBusted failed.");
+            Assert.AreEqual(expectedScore == 21, hand.Is21, "Check Is21 failed.");
+            Assert.AreEqual(expectedScore == 21, hand.IsBlackjack, "Check IsBlackjack failed.");
+
+            Console.WriteLine($"Initial Score: {hand.Score()}. Cards in hand: {hand}");
+
+            // Act 2
+            int count = 1;
+            while (!hand.IsBusted && !hand.Is21)
+            {
+                BlackjackCard newCard = deck.DealCard();
+                hand.AddCard(newCard);
+                handOfCards.Add(newCard);
+
+                // Assert updated hand
+                Assert.AreEqual(52, deck.TotalCards, "Incorrect total number of cards in the deck.");
+                Assert.AreEqual(50 - count, deck.RemainingCards, "Incorrect remaining number of cards in the deck.");
+                Assert.IsTrue(handOfCards.SequenceEqual(hand.Cards));
+
+                int expectedMinScore = 0;
+                int expectedMaxScore = 0;
+                foreach (BlackjackCard card in handOfCards)
+                {
+                    expectedMinScore += card.Value;
+                    expectedMaxScore += card.MaxValue;
+                }
+                expectedScore = expectedMaxScore > 21 ? expectedMinScore : expectedMaxScore;
+                Assert.AreEqual(expectedScore, hand.Score(), "Incorrect score calculated.");
+                Assert.AreEqual(expectedScore > 21, hand.IsBusted, "Check IsBusted failed.");
+                Assert.AreEqual(expectedScore == 21, hand.Is21, "Check Is21 failed.");
+                Assert.IsFalse(hand.IsBlackjack, "Check IsBlackjack failed.");
+
+                Console.WriteLine($"Round {count} Score: {hand.Score()}. Cards in hand: {hand}");
+
+                count++;
+            }
+
+            // Act 3
+            deck.ResetDeck();
+
+            // Assert after deck reset
+            Assert.AreEqual(52, deck.TotalCards, "Incorrect total number of cards after deck reset.");
+            Assert.AreEqual(52, deck.RemainingCards, "Incorrect remaining number of cards after deck reset.");
+        }
+    }
+}
