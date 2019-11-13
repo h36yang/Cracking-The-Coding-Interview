e22047842fd04316b2f86682353a39feb338d62d
Jack Yang <h36yang@gmail.com>
2019-11-12 23:20:51 -0500

    Question 10.3

---
+++ 010_SortingAndSearching/10.3_SearchInRotatedArray.cs
@@ -0,0 +1,83 @@
+﻿namespace _010_SortingAndSearching
+{
+    /// <summary>
+    /// 10.3) Search in Rotated Array:
+    /// Given a sorted array of n integers that has been rotated an unknown number of times, write code to find an element in the array.
+    /// You may assume that the array was originally sorted in increasing order.
+    /// </summary>
+    public static class Question_10_3
+    {
+        /// <summary>
+        /// Modified Binary Search
+        /// <para>Time Complexity: worst case O(n), average case O(log(n))</para>
+        /// <para>Space Complexity: O(log(n)) - recursion depth</para>
+        /// </summary>
+        /// <param name="rotatedArr"></param>
+        /// <param name="item"></param>
+        /// <returns></returns>
+        public static int Search(int[] rotatedArr, int item)
+        {
+            if (rotatedArr == null || rotatedArr.Length == 0)
+            {
+                return -1;
+            }
+            return Search(rotatedArr, item, 0, rotatedArr.Length - 1);
+        }
+
+        private static int Search(int[] rotatedArr, int item, int start, int end)
+        {
+            if (start > end)
+            {
+                return -1;
+            }
+
+            int mid = (start + end) / 2;
+            if (rotatedArr[mid] == item)
+            {
+                return mid;
+            }
+
+            if (rotatedArr[start] < rotatedArr[mid]) // left side is normally ordered
+            {
+                if (rotatedArr[start] <= item && item < rotatedArr[mid]) // item within left side
+                {
+                    return Search(rotatedArr, item, start, mid - 1);
+                }
+                else // item within right side
+                {
+                    return Search(rotatedArr, item, mid + 1, end);
+                }
+            }
+            else if (rotatedArr[start] > rotatedArr[mid]) // right side is normally ordered
+            {
+                if (rotatedArr[mid] < item && item <= rotatedArr[end]) // item within right side
+                {
+                    return Search(rotatedArr, item, mid + 1, end);
+                }
+                else // item within left side
+                {
+                    return Search(rotatedArr, item, start, mid - 1);
+                }
+            }
+            else // left or right side repeats
+            {
+                if (rotatedArr[mid] != rotatedArr[end]) // item within right side
+                {
+                    return Search(rotatedArr, item, mid + 1, end);
+                }
+                else // can't determine - search both sides
+                {
+                    int resultIndex = Search(rotatedArr, item, start, mid - 1);
+                    if (resultIndex == -1)
+                    {
+                        return Search(rotatedArr, item, mid + 1, end);
+                    }
+                    else
+                    {
+                        return resultIndex;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ 010_SortingAndSearchingTest/10.3_SearchInRotatedArrayTest.cs
@@ -0,0 +1,28 @@
+using _010_SortingAndSearching;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace _010_SortingAndSearchingTest
+{
+    [TestClass]
+    public class Question_10_3_Test
+    {
+        [DataTestMethod]
+        [DataRow(null, 0, -1)]
+        [DataRow(new int[0], 1, -1)]
+        [DataRow(new int[] { 5, 7, 10, 14, 15, 16, 19, 20, 25, 1, 3, 4 }, 4, 11)]
+        [DataRow(new int[] { 5, 7, 10, 14, 15, 16, 19, 20, 25, 1, 3, 4 }, 5, 0)]
+        [DataRow(new int[] { 5, 7, 10, 14, 15, 16, 19, 20, 25, 1, 3, 4 }, 1, 9)]
+        [DataRow(new int[] { 15, 16, 19, 20, 25, 1, 3, 4, 5, 7, 10, 14 }, 5, 8)]
+        [DataRow(new int[] { 25, 1, 3, 4, 5, 7, 10, 14, 15, 16, 19, 20 }, 25, 0)]
+        [DataRow(new int[] { 2, 2, 2, 2, 3, 4 }, 4, 5)]
+        [DataRow(new int[] { 2, 2, 2, 3, 4, 2 }, 4, 4)]
+        public void SearchTest(int[] testArray, int testItem, int expectedIndex)
+        {
+            // Act
+            int resultIndex = Question_10_3.Search(testArray, testItem);
+
+            // Assert
+            Assert.AreEqual(expectedIndex, resultIndex, "Search test failed.");
+        }
+    }
+}
