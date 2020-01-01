255a9a6f17e90e7159cbc569ee077153e3098c6c
Jack Yang <h36yang@gmail.com>
2020-01-01 01:21:35 -0500

    Question 16.5 (fix)

--- 016_Moderate/16.05_FactorialZeros.cs
+++ 016_Moderate/16.05_FactorialZeros.cs
@@ -1,6 +1,4 @@
-﻿using System;
-
-namespace _016_Moderate
+﻿namespace _016_Moderate
 {
     /// <summary>
     /// 16.5) Factorial Zeros:
@@ -9,36 +7,58 @@ namespace _016_Moderate
     public static class Question_16_05
     {
         /// <summary>
-        /// Loop through numbers and count the number of zeros/twos/fives
+        /// Loop through numbers and count the factors of 5
         /// <para>Time Complexity: O(n)</para>
         /// <para>Space Complexity: O(1)</para>
         /// </summary>
         /// <param name="n"></param>
         /// <returns></returns>
-        public static int FindFactorialTrailingZeros(int n)
+        public static int CountFactorialTrailingZeros(int n)
         {
-            int zeroCounter = 0;
-            int twoCounter = 0;
-            int fiveCounter = 0;
+            if (n < 0)
+            {
+                return -1;
+            }
+
+            int count = 0;
             for (int i = n; i > 1; i--)
             {
-                int lastDigit = i % 10;
-                switch (lastDigit)
-                {
-                    case 0:
-                        zeroCounter++;
-                        break;
-                    case 2:
-                        twoCounter++;
-                        break;
-                    case 5:
-                        fiveCounter++;
-                        break;
-                    default:
-                        break;
-                }
+                count += CountFactorsOf5(i);
             }
-            return zeroCounter + Math.Min(twoCounter, fiveCounter);
+            return count;
+        }
+
+        private static int CountFactorsOf5(int n)
+        {
+            int count = 0;
+            while (n % 5 == 0)
+            {
+                count++;
+                n /= 5;
+            }
+            return count;
+        }
+
+        /// <summary>
+        /// Count number of factors of 5, 25, 125, etc.
+        /// <para>Time Complexity: O(m) where m is number of multiples of 5 in n</para>
+        /// <para>Space Complexity: O(1)</para>
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        public static int CountFactorialTrailingZerosOptimized(int n)
+        {
+            if (n < 0)
+            {
+                return -1;
+            }
+
+            int count = 0;
+            for (int i = 5; n / i > 0; i *= 5)
+            {
+                count += n / i;
+            }
+            return count;
         }
     }
 }
--- 016_ModerateTest/16.05_FactorialZerosTest.cs
+++ 016_ModerateTest/16.05_FactorialZerosTest.cs
@@ -1,5 +1,6 @@
 using _016_Moderate;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 
 namespace _016_ModerateTest
 {
@@ -7,6 +8,8 @@ namespace _016_ModerateTest
     public class Question_16_05_Test
     {
         [DataTestMethod]
+        [DataRow(-1, -1)]
+        [DataRow(0, 0)]
         [DataRow(1, 0)]
         [DataRow(2, 0)]
         [DataRow(5, 1)]
@@ -15,13 +18,22 @@ namespace _016_ModerateTest
         [DataRow(14, 2)]
         [DataRow(15, 3)]
         [DataRow(20, 4)]
+        [DataRow(25, 6)]
         public void FindFactorialTrailingZerosTest(int testNumber, int expectedZeros)
         {
             // Act
-            int resultZeros = Question_16_05.FindFactorialTrailingZeros(testNumber);
+            var time1 = DateTime.Now;
+            int resultZeros1 = Question_16_05.CountFactorialTrailingZeros(testNumber);
+            var time2 = DateTime.Now;
+            int resultZeros2 = Question_16_05.CountFactorialTrailingZerosOptimized(testNumber);
+            var time3 = DateTime.Now;
+
+            Console.WriteLine($"Runtime 1: {time2.Subtract(time1).TotalMilliseconds} ms");
+            Console.WriteLine($"Runtime 2: {time3.Subtract(time2).TotalMilliseconds} ms");
 
             // Assert
-            Assert.AreEqual(expectedZeros, resultZeros, "Number of trailing zeros does not match.");
+            Assert.AreEqual(expectedZeros, resultZeros1, "Number of trailing zeros does not match.");
+            Assert.AreEqual(expectedZeros, resultZeros2, "Number of trailing zeros does not match - optimized.");
         }
     }
 }
