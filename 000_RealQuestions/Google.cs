ac7c287d279359b65552184c802b33561fc81c3b
Jack Yang <h36yang@gmail.com>
2020-02-29 16:53:56 -0500

    Added real Google OA question

---
+++ 000_RealQuestions/Google.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _000_RealQuestions
+{
+    public static class Google
+    {
+        /// <summary>
+        /// Google | OA | Min Days to Bloom
+        /// </summary>
+        /// <param name="roses">Array of roses, <c>roses[i]</c> means rose <c>i</c> will bloom on day <c>roses[i]</c></param>
+        /// <param name="k">The minimum number of adjacent bloom roses required for a bouquet</param>
+        /// <param name="n">The number of bouquets we need</param>
+        /// <returns>The earliest day that we can get n bouquets of roses</returns>
+        public static int MinDaysBloom(int[] roses, int k, int n)
+        {
+            int[] maxSlidingWindow = MaxSlidingWindow(roses, k);
+
+            var runningMin = new int[maxSlidingWindow.Length];
+            runningMin[^1] = maxSlidingWindow[^1];
+            for (int i = runningMin.Length - 2; i >= 0; i--)
+            {
+                runningMin[i] = Math.Min(runningMin[i + 1], maxSlidingWindow[i]);
+            }
+
+            var memo = new int[maxSlidingWindow.Length, n + 1];
+            return MinDaysBloom(maxSlidingWindow, n, 0, runningMin, memo);
+        }
+
+        private static int MinDaysBloom(int[] maxSlidingWindow, int n, int start, int[] runningMin, int[,] memo)
+        {
+            if (start >= maxSlidingWindow.Length)
+            {
+                return 0;
+            }
+
+            if (n == 1)
+            {
+                return runningMin[start];
+            }
+
+            if (memo[start, n] > 0)
+            {
+                return memo[start, n];
+            }
+
+            int minDays = int.MaxValue;
+            int localMinDays = int.MaxValue;
+            for (int i = start; i <= maxSlidingWindow.Length - n; i++)
+            {
+                localMinDays = Math.Min(localMinDays, maxSlidingWindow[i]);
+                if (localMinDays < minDays)
+                {
+                    minDays = Math.Min(minDays, Math.Max(localMinDays, MinDaysBloom(maxSlidingWindow, n - 1, i + 1, runningMin, memo)));
+                }
+            }
+            return minDays;
+        }
+
+        private static int[] MaxSlidingWindow(int[] roses, int k)
+        {
+            var result = new int[roses.Length - k + 1];
+            var deque = new LinkedList<int>();
+            for (int i = 0; i < roses.Length; i++)
+            {
+                while (deque.Count > 0 && roses[deque.Last.Value] < roses[i])
+                {
+                    deque.RemoveLast();
+                }
+
+                while (deque.Count > 0 && deque.First.Value <= i - k)
+                {
+                    deque.RemoveFirst();
+                }
+
+                deque.AddLast(i);
+
+                if (i >= k - 1)
+                {
+                    result[i - k + 1] = roses[deque.First.Value];
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ 000_RealQuestionsTest/GoogleTest.cs
@@ -0,0 +1,20 @@
+﻿using _000_RealQuestions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace _000_RealQuestionsTest
+{
+    [TestClass]
+    public class GoogleTest
+    {
+        [DataTestMethod]
+        [DataRow(new int[] { 1, 2, 4, 9, 3, 4, 1 }, 2, 2, 4)]
+        public void MinDaysBloomTest(int[] roses, int k, int n, int expected)
+        {
+            // Act
+            int result = Google.MinDaysBloom(roses, k, n);
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
